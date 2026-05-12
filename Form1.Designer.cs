23d1eceb232d8b9846396aee92e4261f1513fa4c
Arif Muradli ADM <arif.muradli@accessbank.az>
2026-05-12 15:36:56 +0400

    1

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace DiskSpace1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 301);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(36, 256);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(209, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(620, 196);
+            this.label3.TabIndex = 9;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::DiskSpace1.Properties.Resources.eng6;
+            this.button4.Location = new System.Drawing.Point(572, 246);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(54, 54);
+            this.button4.TabIndex = 11;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::DiskSpace1.Properties.Resources.aze1;
+            this.button3.Location = new System.Drawing.Point(497, 246);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(54, 54);
+            this.button3.TabIndex = 10;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 362);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DiskSpace error";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DiskSpace1
+{
+    public partial class Form1 : Form
+    {
+        // Define an event handler for DiskSpaceLow event
+        public event EventHandler DiskSpaceLow;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.TopMost = true;
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            RefreshDiskSpace();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Example URL
+            string url = "https://abasd.accessbank.az/servicedesk/customer/portal/1/create/383";
+
+            // Open the URL in the default browser
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(url) { UseShellExecute = true });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Call method to refresh disk space information
+            RefreshDiskSpace();
+        }
+
+        private void RefreshDiskSpace()
+        {
+            try
+            {
+                // Get information about the system drive
+                DriveInfo driveInfo = new DriveInfo(Path.GetPathRoot(Environment.SystemDirectory));
+
+                // Calculate the disk space usage percentage
+                double totalSpace = driveInfo.TotalSize;
+                double freeSpace = driveInfo.TotalFreeSpace;
+                double usedSpace = totalSpace - freeSpace;
+                double percentageUsed = (usedSpace / totalSpace) * 100;
+                double remainingSpace = freeSpace / (1024 * 1024 * 1024); // Convert to GB
+                double percentageRemaining = 100 - percentageUsed;
+
+
+                // Update labels and progress bar
+                //label1.Text = $"Free Space: {freeSpace / (1024 * 1024 * 1024):N2} GB";
+                //label1.Text = $"Free Space: {freeSpace / (1024 * 1024 * 1024):N2} GB ({percentageUsed:N2}%)";
+                label1.Text = $"Free Space: {remainingSpace:N2} GB ({percentageRemaining:N2}%)";
+                label2.Text = $"Total Space: {totalSpace / (1024 * 1024 * 1024):N2} GB";
+                progressBar1.Value = (int)percentageUsed;
+
+                // Change the color of the progress bar based on remaining percentage
+                if (percentageRemaining < 10)
+                {
+                    progressBar1.ForeColor = Color.Red;
+                }
+                else
+                {
+                    progressBar1.ForeColor = Color.Green;
+                }
+
+                // Check if free percentage is below 10%
+                if (percentageRemaining < 10)
+                {
+                    // Trigger the DiskSpaceLow event
+                    OnDiskSpaceLow();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle error
+                MessageBox.Show($"Error occurred while refreshing disk space information: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Raise the DiskSpaceLow event
+        protected virtual void OnDiskSpaceLow()
+        {
+            DiskSpaceLow?.Invoke(this, EventArgs.Empty);
+        }
+        private string label3OriginalText = "Diqqət!\r\nSizin komputerinizin sistem diskində boş yer 10%-dən aşağıdır.\r\nZəhmət olmasa, sistem diskinizdə (\"C\" diski) olan böyükhəcmli fayllarınızı digər disklərə (\"D\" və s.) köçürün və kömək lazım olarsa Helpdesk-ə müraciət edin.";
+        private string label3EnglishText = "Attention!\r\nThe free space in your computer is below 10%.\r\nPlease move large files from your system disk (C:) to other disks (such as D:). \r\nIf you need assistance please ask Helpdesk.";
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label3.Text = label3OriginalText; // Set label3 text to original language
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label3.Text = label3EnglishText; // Set label3 text to English
+        }
+    }
+}
